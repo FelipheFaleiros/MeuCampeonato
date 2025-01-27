6a0eb50239a1090aa498fa2065dd82a122be4935
Feliphe <feliphefaleiros@hotmail.com>
2025-01-27 06:26:19 -0300

    ajuste DB

--- BackEnd/db/db.js
+++ BackEnd/db/db.js
@@ -2,11 +2,23 @@
 const mysql = require('mysql') 
 //escolhido o mysql por se tratar de um pequeno projeto o qual esse sgbd se da melhor para um sistema do porte que está sendo desenvolvido
 
-const db = mysql.createConnection({ //fazer conexao ao db
+//conexão
+const db = mysql.createPool({ //fazer conexao ao db
     host: "localhost",
     user: "root",
     password: "D]agBb]A+6(bA1??", //vai depender se tem senha para entrar no mysql
     database:"meucampeonato"
-})
+}) 
+
+// db.connect( function(err){
+//     console.log("Conexão ao banco de dados realizada com sucesso")
+
+
+//     if (erro){
+//         console.log("Falha na conexão ao banco de dados")
+//         console.error('error connecting: ' + err.stack);
+//     }
+// });
+
 
 module.exports = db
